0d29f27cd41dd89a6c5900b0aaab9e2804b9b4ce
ahmedEssamFawaz <49008995+ahmedEssamFawaz@users.noreply.github.com>
2023-03-18 22:37:28 +0200

    first init and structure

---
+++ components/Home/MoviesList.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col md="4">
+      <HomeSingleMovieCard />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <HomeMoviesList/>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
